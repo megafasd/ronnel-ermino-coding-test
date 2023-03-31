78ecf3e08c992608c799479b4102a60ba88a161a
Ronnel Ermino <201811748@feualabang.edu.ph>
2023-03-31 23:41:47 +0800

    fixed namespace error for ProductController class and added missing semicolon

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Models\Product
+use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
+#use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------

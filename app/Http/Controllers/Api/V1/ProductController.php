63f063bfc04c5b95f3f19ff0bd2c933d0b5c9f89
Ronnel Ermino <201811748@feualabang.edu.ph>
2023-04-01 15:07:03 +0800

    added post function for Create Product

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\ProductResource;
 use App\Http\Resources\V1\ProductCollection;
 use App\Services\V1\ProductQuery;
+use App\Http\Requests\V1\StoreProductRequest;
 
 class ProductController extends Controller
 {
@@ -36,9 +37,9 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreProductRequest $request)
     {
-        //
+        return new ProductResource(Product::create($request->all()));
     }
 
     /**
---
+++ app/Http/Requests/V1/StoreProductRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'id' => ['required'],
+            'name' => ['required', 'max:255'],
+            'description' => ['required'],
+            'price' => ['required', 'regex:/^\d*(\.\d{1,2})?$/'],
+        ];
+    }
+}
